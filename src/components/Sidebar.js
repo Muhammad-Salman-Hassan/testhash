d974e0eaf37196837b6515199f2c80f0bbdb44ee
unknown <msalmanhassan288311@gmail.com>
2023-01-06 23:05:18 +0500

    Adding Styling

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,50 @@
 import React from "react";
 import "./sidebar.css";
 import logo from "../Images/logo.png";
+import dashboard from "../Images/Vector.png"
+
+import buypass from "../Images/Vector (4).png"
+import stake from "../Images/Vector (2).png"
+import bond from "../Images/Vector (3).png"
+import Analytics from "../Images/Vector (5).png"
+import document from "../Images/Vector (6).png"
+import twitter from "../Images/twitter.png"
+import discord from "../Images/discord.png"
+import telegram from "../Images/telegram.png"
 const Sidebar = () => {
   return (
     <div className="sidebar ">
-      <div className="sidebar_wrapper w-100 h-100 ">
+      <div className="sidebar_wrapper w-100  ">
         <div className="logo_wrapper">
           <img src={logo} alt="" />
         </div>
         {/* =========>>Links<<<<<<<<<<< */}
         <ul>
-          <li>DashBoard</li>
-          <li>Analytics</li>
-          <li>Stake</li>
+          <li><img src={dashboard} alt="" /> <span className="dashboard_link">DashBoard</span></li>
+          <li><img src={Analytics} alt="" /> <span className="dashboard_link">Analytics</span></li>
+          <li><img src={stake} alt="" /> <span className="dashboard_link">Stake</span></li>
           <li>
-            Bond
-            <ul>
-              <li>XIV-BNB</li>
-            </ul>
+          <img src={bond} alt="" /> <span className="dashboard_link">Bond</span>
+            
           </li>
-          <li></li>
+          <ul className="bondToggle">
+            <span className="Bond_Discount">Bond Discount</span>
+              <li><span>XIV-BNB LP</span> <span>-16.28%</span></li>
+              <li><span>PASS-BNB LP</span> <span>-16.28%</span></li>
+              <li><span>XIV</span> <span>-16.28%</span></li>
+              <li><span>BNB</span> <span>-16.28%</span></li>
+              <li><span>BUSD</span> <span>-16.28%</span></li>
+            </ul>
+          <li><img src={buypass} alt="" /> <span className="dashboard_link">Buy Pass</span></li>
+          <li><img src={Analytics} alt="" /> <span className="dashboard_link">Price Chart</span></li>
+          <li><img src={document} alt="" /> <span className="dashboard_link">Documentation</span></li>
         </ul>
+
+        <div className="social_wrapper">
+          <span><img src={twitter} alt="" /></span>
+          <span><img src={discord} alt="" /></span>
+          <span><img src={telegram} alt="" /></span>
+        </div>
       </div>
     </div>
   );
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Card from '../../components/card/Card'
 import "./main.css"
+import dummydata from "../../dummy.json"
 const Main = () => {
+  console.log(dummydata)
   return (
-    <div className='Main'>Main</div>
+    <div className='Main'>
+      <div className="main_wrapper">
+        <div className="nav_btn_wrapper">
+          <button className='buy_pass'>Buy Pass</button>
+          <button className='connect_wallet'>Connect Wallet</button>
+        </div>
+        <h4 className='text-center mt-5 epoch_staking'>Epoch Staking</h4>
+        <div className="cards_wrapper d-flex">
+          {dummydata.data.map((elem)=>{
+            return (
+              <Card className="mycard">
+                <h6 className="card_heading">{elem.heading}</h6>
+                <span>{elem.value}</span>
+              </Card>
+            )
+          })}
+         
+         
+         
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
